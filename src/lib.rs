93c0438adcb8d8d310a16940eac73f3055a7f2c0
Kagami Hiiragi <kagami@genshiken.org>
2015-11-07 03:14:18 +0300

    Some decoding fixes

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,31 @@ use std::io;
 use std::io::Read;
 
 const MAX_PARAMS_SIZE: usize = 1024;
-const Y4M_MAGICK: &'static [u8] = b"YUV4MPEG2 ";
+const FILE_MAGICK: &'static [u8] = b"YUV4MPEG2 ";
 const FRAME_MAGICK: &'static [u8] = b"FRAME";
 const TERMINATOR: u8 = 0x0A;
 const SEPARATOR: u8 = b' ';
 
 #[derive(Debug)]
 pub enum Error {
+    /// End of the file. Technically not an error, but it's easier to process
+    /// that way.
     EOF,
     // TODO(Kagami): Better granularity of parse errors.
     ParseError,
     IoError(io::Error),
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error { Error::IoError(err) }
+}
+
+impl From<str::Utf8Error> for Error {
+    fn from(_: str::Utf8Error) -> Error { Error::ParseError }
+}
+
+impl From<num::ParseIntError> for Error {
+    fn from(_: num::ParseIntError) -> Error { Error::ParseError }
 }
 
 trait EnhancedRead {
@@ -26,8 +40,8 @@ trait EnhancedRead {
 
 impl<R: Read> EnhancedRead for R {
     // Current implementation does one `read` call per byte. This might be a
-    // bit slow for long headers but it simplifies other things: we don't need
-    // to check whether start of the next frame is already read and so on.
+    // bit slow for long headers but it simplifies things: we don't need to
+    // check whether start of the next frame is already read and so on.
     fn read_until(&mut self, ch: u8, buf: &mut [u8]) -> Result<usize, Error> {
         let mut collected = 0;
         while collected < buf.len() {
@@ -72,18 +86,6 @@ pub enum Colorspace {
     C420paldv,
 }
 
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Error { Error::IoError(err) }
-}
-
-impl From<str::Utf8Error> for Error {
-    fn from(_: str::Utf8Error) -> Error { Error::ParseError }
-}
-
-impl From<num::ParseIntError> for Error {
-    fn from(_: num::ParseIntError) -> Error { Error::ParseError }
-}
-
 pub struct Decoder<R: Read> {
     reader: R,
     params_buf: Vec<u8>,
@@ -100,13 +102,14 @@ impl<R: Read> Decoder<R> {
     pub fn new(mut reader: R) -> Result<Decoder<R>, Error> {
         let mut params_buf = vec![0;MAX_PARAMS_SIZE];
         let end_params_pos = try!(reader.read_until(TERMINATOR, &mut params_buf));
-        if end_params_pos < Y4M_MAGICK.len() || !params_buf.starts_with(Y4M_MAGICK) {
+        if end_params_pos < FILE_MAGICK.len() || !params_buf.starts_with(FILE_MAGICK) {
             return Err(Error::ParseError);
         }
-        let raw_params = (&params_buf[Y4M_MAGICK.len()..end_params_pos]).to_owned();
+        let raw_params = (&params_buf[FILE_MAGICK.len()..end_params_pos]).to_owned();
         let mut width = 0;
         let mut height = 0;
         let mut csp = None;
+        // We shouldn't convert it to string because encoding is unspecified.
         for param in raw_params.split(|&b| b == SEPARATOR) {
             if param.len() < 1 { continue }
             let (name, value) = (param[0], &param[1..]);
@@ -166,7 +169,7 @@ impl<R: Read> Decoder<R> {
         }
     }
 
-    /// Iterate over frames, without heap allocations. End of input is
+    /// Iterate over frames, without extra heap allocations. End of input is
     /// indicated by `Error::EOF`.
     pub fn next_frame<'a>(&'a mut self) -> Result<Frame<'a>, Error> {
         let end_params_pos = try!(self.reader.read_until(TERMINATOR, &mut self.params_buf));
@@ -174,7 +177,16 @@ impl<R: Read> Decoder<R> {
             return Err(Error::ParseError);
         }
         // We don't parse frame params currently but user has access to them.
-        let raw_params = (&self.params_buf[FRAME_MAGICK.len()..end_params_pos]).to_owned();
+        let start_params_pos = FRAME_MAGICK.len();
+        let raw_params = if end_params_pos - start_params_pos > 0 {
+            // Check for extra space.
+            if self.params_buf[start_params_pos] != SEPARATOR {
+                return Err(Error::ParseError);
+            }
+            Some((&self.params_buf[start_params_pos+1..end_params_pos]).to_owned())
+        } else {
+            None
+        };
         try!(self.reader.read_exact(&mut self.frame_buf));
         Ok(Frame {
             planes: [
@@ -206,14 +218,14 @@ impl<R: Read> fmt::Debug for Decoder<R> {
 #[derive(Debug)]
 pub struct Frame<'a> {
     planes: [&'a [u8];3],
-    raw_params: Vec<u8>,
+    raw_params: Option<Vec<u8>>,
 }
 
 impl<'a> Frame<'a> {
     pub fn get_y_plane(&self) -> &[u8] { self.planes[0] }
     pub fn get_u_plane(&self) -> &[u8] { self.planes[1] }
     pub fn get_v_plane(&self) -> &[u8] { self.planes[2] }
-    pub fn get_raw_params(&self) -> &[u8] { &self.raw_params }
+    pub fn get_raw_params(&self) -> Option<&[u8]> { self.raw_params.as_ref().map(|v| &v[..]) }
 }
 
 /// Create a new decoder instance. Alias for `Decoder::new`.
