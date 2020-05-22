0ccda283adc7ac1c81a36ca702a7c381366a1392
cN3rd <subs.in.tokyo@gmail.com>
2020-05-22 21:58:15 +0300

    Implement std::error::Error trait for y4m::Error
    Allows smoother integration to libraries
    which assume errors are derived from std::error::Error,
    like `anyhow`.

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,32 @@ pub enum Error {
     OutOfMemory,
 }
 
+impl std::error::Error for crate::Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match *self {
+            Error::EOF => None,
+            Error::BadInput => None,
+            Error::UnknownColorspace => None,
+            Error::ParseError(ref err) => Some(err),
+            Error::IoError(ref err) => Some(err),
+            Error::OutOfMemory => None,
+        }
+    }
+}
+
+impl fmt::Display for crate::Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::EOF => write!(f, "End of file"),
+            Error::BadInput => write!(f, "Bad input parameters provided"),
+            Error::UnknownColorspace => write!(f, "Bad input parameters provided"),
+            Error::ParseError(ref err) => err.fmt(f),
+            Error::IoError(ref err) => err.fmt(f),
+            Error::OutOfMemory => write!(f, "Out of memory (limits exceeded)"),
+        }
+    }
+}
+
 /// Granular ParseError Definiations
 pub enum ParseError {
     /// Error reading y4m header
